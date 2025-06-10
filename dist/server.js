049c691415f6162a4915ed2612f20c6ed7859284
Lucas <40727001+HelloMeow10@users.noreply.github.com>
2025-06-10 12:32:00 -0300

    sql

--- dist/server.js
+++ dist/server.js
@@ -12,6 +12,7 @@ const db_1 = require("./config/db");
 const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const errorHandler_1 = require("./middlewares/errorHandler");
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config(); // Carga variables de entorno desde .env:contentReference[oaicite:8]{index=8}
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
@@ -19,9 +20,18 @@ const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use((0, morgan_1.default)('dev'));
+// Servir archivos estáticos de la carpeta css
+app.use('/css', express_1.default.static(path_1.default.join(__dirname, '../css')));
+// Servir archivos estáticos de la carpeta html
+app.use(express_1.default.static(path_1.default.join(__dirname, '../html')));
+// Servir archivos estáticos de la carpeta js
+app.use('/js', express_1.default.static(path_1.default.join(__dirname, '../js')));
 // Rutas
 app.use('/api/products', productRoutes_1.default);
 app.use('/api/auth', authRoutes_1.default);
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../html/inicio.html'));
+});
 // Middleware de manejo de errores (al final de todos)
 app.use(errorHandler_1.errorHandler);
 // Conectar a la base de datos y arrancar el servidor
